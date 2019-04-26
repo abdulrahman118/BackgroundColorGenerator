fbc890658095ee6a286d88f1cb4fce9e4eb2f684
abdulrahman118 <abdulrahman118@gmail.com>
2019-04-26 23:13:25 +0530

    Random button added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var random = document.querySelector(".wrapper");
+
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +16,16 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function generateRandomColor(){
+	color1.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+setGradient();
+
 color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);color2.addEventListener("input", setGradient);
+
+random.addEventListener("click",generateRandomColor);
